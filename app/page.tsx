a53aff290fcb41d8cd23fdbdc66964132b004732
klhaug <kristian@snougle.no>
2024-11-15 20:13:50 +0100

    learning about fonts

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
       <main className="flex min-h-screen flex-col items-center justify-between p-14">
         <h1>Coffee Connoisseur</h1>
+        <h2>The best coffee shops in town</h2>
+        <button>This is a button</button>
       </main>
   );
 }
