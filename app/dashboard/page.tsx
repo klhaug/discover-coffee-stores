030a816c8fc424bcb4fb51fed3c1a5ed424c9ac6
klhaug <kristian@snougle.no>
2024-11-16 09:15:42 +0100

    testing routing and linking

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Dashboard() {
+    return (
+        <div>
+            <h1>Test</h1>
+            <Link href="/">Back</Link>
+        </div>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
       <main className="flex min-h-screen flex-col items-center justify-between p-14">
         <h1>Coffee Connoisseur</h1>
         <h2>The best coffee shops in town</h2>
-        <button>This is a button</button>
+        <Link href="/dashboard">
+          <button>This is a button</button>
+        </Link>
       </main>
   );
 }
