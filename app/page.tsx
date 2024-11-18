bca895513ed96041ccd74973933ce568451d501a
klhaug <kristian@snougle.no>
2024-11-18 07:09:17 +0100

    saving

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
 import Banner from "@/components/banner.client";
+import Link from "next/link";
+import Image from "next/image";
 
 export default function Home() {
+  const coffeeStoreId = 'dark-horse-coffe'
   return (
       <main className="flex min-h-screen flex-col items-center justify-between p-14">
         <Banner />
+        <Link href={`./coffee-store/${coffeeStoreId}`}>Coffee</Link>
+        <Image 
+          src= "/static/bannerPic.webp" 
+          alt ="illustration of a woman drinking coffee"
+          height={500}
+          width={500}
+          />
       </main>
   );
 }
