c16a506aa317e3390e865b1fa87674d4e112fced
klhaug <kristian@snougle.no>
2024-11-19 06:50:57 +0100

    saving, we made a card component!

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,25 @@
 import Banner from "@/components/banner.client";
 import Link from "next/link";
 import Image from "next/image";
+import Card from "@/components/card.server";
 
 export default function Home() {
+ 
   const coffeeStoreId = 'dark-horse-coffe'
+ 
   return (
-      <main className="flex min-h-screen flex-col items-center justify-between p-14">
+    <div className="mb-56">
+      <main className="mx-auto mt-10 max-w-6xl px-4">
         <Banner />
-        <Link href={`./coffee-store/${coffeeStoreId}`}>Coffee</Link>
-        <Image 
-          src= "/static/bannerPic.webp" 
-          alt ="illustration of a woman drinking coffee"
-          height={500}
-          width={500}
-          />
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-2 lg:grid-cols-3 lg:gap-6">
+          <Card 
+            name="Dark Horse Coffee"
+            imgUrl="/static/bannerPic.webp"
+            href={`/coffee-store/${coffeeStoreId}`}
+            />
+          </div>
       </main>
+    </div>
+      
   );
 }
